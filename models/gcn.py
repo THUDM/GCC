374c32dbda796e6465c2a6ee2b74e1d46b74513a
Qibin Chen <chenqibin422@gmail.com>
2019-12-13 23:49:00 +0800

    Fix GCN Model

--- models/gcn.py
+++ models/gcn.py
@@ -5,48 +5,37 @@
 # Create Time: 2019/12/13 15:38
 # TODO:
 
+import dgl
 import dgl.function as fn
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from dgl.model_zoo.chem.gnn import GCNLayer
 
-gcn_msg = fn.copy_src(src='h', out='m')
-gcn_reduce = fn.sum(msg='m', out='h')
 
-class NodeApplyModule(nn.Module):
-    def __init__(self, in_feats, out_feats, activation):
-        super(NodeApplyModule, self).__init__()
-        self.linear = nn.Linear(in_feats, out_feats)
-        self.activation = activation
-
-    def forward(self, node):
-        h = self.linear(node.data['h'])
-        if self.activation is not None:
-            h = self.activation(h)
-        return {'h' : h}
-
-class GCNLayer(nn.Module):
-    def __init__(self, in_feats, out_feats, activation):
-        super(GCNLayer, self).__init__()
-        self.apply_mod = NodeApplyModule(in_feats, out_feats, activation)
-
-    def forward(self, g, feature):
-        g.ndata['h'] = feature
-        g.update_all(gcn_msg, gcn_reduce)
-        g.apply_nodes(func=self.apply_mod)
-        return g.ndata.pop('h')
-
-class GCN(nn.Module):
+class UnsupervisedGCN(nn.Module):
     def __init__(self, hidden_size=64, num_layer=2):
-        super(GCN, self).__init__()
-        self.layers = nn.Sequential(*[
-                GCNLayer(hidden_size, hidden_size, F.relu if i+1 < num_layer else None)
-                    for i in range(num_layer)
-            ])
+        super(UnsupervisedGCN, self).__init__()
+        self.layers = nn.ModuleList(
+            [
+                GCNLayer(
+                    hidden_size, hidden_size, F.relu if i + 1 < num_layer else nn.Sequential()
+                )
+                for i in range(num_layer)
+            ]
+        )
 
     def forward(self, g, feature):
-        x = self.layers(g, feature)
-        return x
+        for layer in self.layers:
+            feats = layer(g, feature)
+        return feats
+
 
 if __name__ == "__main__":
-	model = GCN()
-	print(model)
+    model = UnsupervisedGCN()
+    print(model)
+    g = dgl.DGLGraph()
+    g.add_nodes(3)
+    g.add_edges([0, 0, 1], [1, 2, 2])
+    feat = torch.rand(3, 64)
+    print(model(g, feat).shape)
