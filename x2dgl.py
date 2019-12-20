a07b2b3d21791c55c572c2c75fde2ed4f769b03f
Jiezhong Qiu <jiezhongqiu@outlook.com>
2019-12-20 15:58:50 +0800

    remove self-loop and 0-degree node, guarantee symmetric graph

--- x2dgl.py
+++ x2dgl.py
@@ -5,14 +5,22 @@
 # Create Time: 2019/12/20 13:39
 # TODO:
 
+import re
 import dgl
 from dgl.data.utils import save_graphs
 import numpy as np
 import scipy.sparse as sp
 import argparse
 import pathlib
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def yuxiao_kdd17_graph_to_dgl(file):
+    logger.info("processing %s", file)
     with open(file, "r") as f:
         n = int(f.readline().split()[1])
         for i in range(n):
@@ -20,19 +28,33 @@ def yuxiao_kdd17_graph_to_dgl(file):
             # so we do not need them
             f.readline()
         m = int(f.readline().split()[1])
-        row, col = [0] * m, [0] * m
+        m_true = 0
+        row, col = [0] * (2*m), [0] * (2*m)
         for i in range(m):
             u, v, _ = list(map(int, f.readline().split()))
-            assert u != v, "contain self-loop"
-            row[i], col[i] = u, v
-    val = [1] * m
-    A = sp.coo_matrix((val, (row, col)))
+            if u == v:
+                continue
+            u, v = min(u, v), max(u, v)
+            row[m_true], col[m_true] = u, v
+            m_true += 1
+            row[m_true], col[m_true] = v, u
+            m_true += 1
+
+    logger.info("raw file has %d nodes and %d edges", n, m)
+    val = [1] * m_true
+    row, col = row[:m_true], col[:m_true]
+    A = sp.coo_matrix((val, (row, col)), shape=(n, n))
     A = A.tocsr()
+    # tocsr will deduplicate edges, and sum up their value
+    A.data = np.ones_like(A.data) # delete this line if not necessary
     sym_err = A - A.T
     sym_check_res = np.all(np.abs(sym_err.data) < 1e-10)  # tune this value
     assert sym_check_res, 'input matrix is not symmetric!!!'
     g = dgl.DGLGraph()
     g.from_scipy_sparse_matrix(A)
+    g.remove_nodes((g.in_degrees() == 0).nonzero().squeeze())
+    logger.info("%d nodes, %d edges, %d self-loop(s) removed, %d zero-degree node(s) removed", g.number_of_nodes(), g.number_of_edges(), 2*m-m_true, n-g.number_of_nodes())
+    logger.info("return graph %s", re.sub("\s+", " ", str(g)))
     return g
 
 if __name__ == "__main__":
@@ -44,4 +66,5 @@ if __name__ == "__main__":
     graph_dir = pathlib.Path(args.graph_dir)
     #  g = yuxiao_kdd17_graph_to_dgl("data_bin/ca-GrQc-SNAP.txtu.lpm.lscc")
     graphs = [yuxiao_kdd17_graph_to_dgl(graph_file) for graph_file in graph_dir.iterdir() if graph_file.is_file()]
+    logger.info("save graphs to %s", args.save_file)
     save_graphs(args.save_file, graphs)
