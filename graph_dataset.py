Fix test; average graph_q graph_k features; add test scripts

graph_dataset.py
@@ -57,7 +57,7 @@ class LoadBalanceGraphDataset(torch.utils.data.IterableDataset):
         workloads = [0] * (num_workers // num_copies)
         graph_sizes = sorted(enumerate(graph_sizes), key=operator.itemgetter(1), reverse=True)
         # Drop top 2 largest graphs
-        # graph_sizes = graph_sizes[2:]
+        graph_sizes = graph_sizes[2:]
         for idx, size in graph_sizes:
             argmin = workloads.index(min(workloads))
             workloads[argmin] += size
@@ -113,15 +113,15 @@ class LoadBalanceGraphDataset(torch.utils.data.IterableDataset):
         return graph_q, graph_k
 
 class GraphDataset(torch.utils.data.Dataset):
-    def __init__(self, rw_hops=64, subgraph_size=64, restart_prob=0.8, hidden_size=32, step_dist=[1.0, 0.0, 0.0]):
+    def __init__(self, rw_hops=64, subgraph_size=64, restart_prob=0.8, positional_embedding_size=32, step_dist=[1.0, 0.0, 0.0]):
         super(GraphDataset).__init__()
         self.rw_hops = rw_hops
         self.subgraph_size = subgraph_size
         self.restart_prob = restart_prob
-        self.hidden_size = hidden_size
+        self.positional_embedding_size = positional_embedding_size
         self.step_dist = step_dist
         assert sum(step_dist) == 1.0
-        assert(hidden_size > 1)
+        assert(positional_embedding_size  > 1)
         #  graphs = []
         graphs, _ = dgl.data.utils.load_graphs("data_bin/dgl/lscc_graphs.bin", [0, 1, 2])
         for name in ["cs", "physics"]:
@@ -188,23 +188,23 @@ class GraphDataset(torch.utils.data.Dataset):
                 g=self.graphs[graph_idx],
                 seed=node_idx,
                 trace=traces[0],
-                hidden_size=self.hidden_size)
+                positional_embedding_size=self.positional_embedding_size)
         graph_k = data_util._rwr_trace_to_dgl_graph(
                 g=self.graphs[graph_idx],
                 seed=other_node_idx,
                 trace=traces[1],
-                hidden_size=self.hidden_size)
+                positional_embedding_size=self.positional_embedding_size)
         return graph_q, graph_k
 
 
 class CogDLGraphDataset(GraphDataset):
-    def __init__(self, dataset, rw_hops=64, subgraph_size=64, restart_prob=0.8, hidden_size=32, step_dist=[1.0, 0.0, 0.0]):
+    def __init__(self, dataset, rw_hops=64, subgraph_size=64, restart_prob=0.8, positional_embedding_size=32, step_dist=[1.0, 0.0, 0.0]):
         self.rw_hops = rw_hops
         self.subgraph_size = subgraph_size
         self.restart_prob = restart_prob
-        self.hidden_size = hidden_size
+        self.positional_embedding_size = positional_embedding_size
         self.step_dist = step_dist
-        assert(hidden_size > 1)
+        assert(positional_embedding_size > 1)
 
         class tmp():
             # HACK

test_graph_moco.py
@@ -33,16 +33,18 @@ def test_moco(train_loader, model, opt):
 
     emb_list = []
     for idx, batch in enumerate(train_loader):
-        graph_q = batch.graph_q
+        graph_q, graph_k = batch.graph_q, batch.graph_k
         bsz = graph_q.batch_size
-        graph_q_feat = graph_q.ndata["x"].cuda(opt.gpu)
-        graph_q_efeat = graph_q.edata['efeat'].cuda(opt.gpu)
+        graph_q.to(torch.device(opt.gpu))
+        graph_k.to(torch.device(opt.gpu))
 
         with torch.no_grad():
-            feat_q = model(graph_q, graph_q_feat, graph_q_efeat)
+            feat_q = model(graph_q)
+            feat_k = model(graph_k)
 
         assert feat_q.shape == (bsz, opt.hidden_size)
-        emb_list.append(feat_q.detach().cpu())
+        # emb_list.append(feat_q.detach().cpu())
+        emb_list.append(((feat_q + feat_k) / 2).detach().cpu())
     return torch.cat(emb_list)
 
 def main(args):
@@ -64,7 +66,7 @@ def main(args):
             rw_hops=args.rw_hops,
             subgraph_size=args.subgraph_size,
             restart_prob=args.restart_prob,
-            hidden_size=args.hidden_size,
+            positional_embedding_size=args.positional_embedding_size,
         )
     train_loader = torch.utils.data.DataLoader(
         dataset=train_dataset,
@@ -91,9 +93,10 @@ def main(args):
                 )
     elif args.model == "mpnn":
         model = UnsupervisedMPNN(
-                node_input_dim=args.hidden_size,
-                edge_input_class=8,
-                edge_input_dim=args.hidden_size,
+                positional_embedding_size=args.positional_embedding_size,
+                max_node_freq=args.max_node_freq,
+                max_edge_freq=args.max_edge_freq,
+                freq_embedding_size=args.freq_embedding_size,
                 output_dim=args.hidden_size,
                 node_hidden_dim=args.hidden_size,
                 edge_hidden_dim=args.hidden_size,
@@ -108,7 +111,7 @@ def main(args):
 
     # optionally resume from a checkpoint
     args.start_epoch = 1
-    model_path = os.path.join(args.model_folder, "current.pth")
+    model_path = os.path.join(args.model_folder, "ckpt_epoch_30.pth")
     if os.path.isfile(model_path):
         print("=> loading checkpoint '{}'".format(model_path))
         checkpoint = torch.load(model_path, map_location="cpu")